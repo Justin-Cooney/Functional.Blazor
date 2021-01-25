43583723291edc8f76690f236797a26881f1ded6
Ryan Marcotte <ryan.marcotte@live.ca>
2021-01-24 23:33:07 -0600

    multi-target netstandard2.1 and net5.0; add bunit tests

---
+++ Functional.Blazor.Components.Tests/OptionMatchTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Bunit;
+using FluentAssertions;
+using Xunit;
+
+namespace Functional.Blazor.Components.Tests
+{
+	public class OptionMatchTests
+	{
+		private const string VALUE = "the option holds a value";
+		private const string NO_VALUE = "the option does not hold a value";
+
+		[Fact]
+		public void DisplaysValueWhenOptionHasValue()
+		{
+			using var context = new TestContext();
+			var cut = context.RenderComponent<OptionMatch<string>>(parameters => parameters
+				.Add(c => c.Option, Option.Some(VALUE))
+				.Add(c => c.Some, s => $"<p>{s}</p>")
+				.Add(c => c.None, $"<p>{NO_VALUE}</p>"));
+
+			cut.Markup.Should().Contain(VALUE);
+		}
+
+		[Fact]
+		public void DisplaysFallbackWhenOptionHasNoValue()
+		{
+			using var context = new TestContext();
+			var cut = context.RenderComponent<OptionMatch<string>>(parameters => parameters
+				.Add(c => c.Option, Option.None<string>())
+				.Add(c => c.Some, s => $"<p>{s}</p>")
+				.Add(c => c.None, $"<p>{NO_VALUE}</p>"));
+
+			cut.Markup.Should().Contain(NO_VALUE);
+		}
+	}
+}
---
+++ Functional.Blazor.Components.Tests/ResultMatchTests.cs
@@ -0,0 +1,36 @@
+﻿using Bunit;
+using FluentAssertions;
+using Xunit;
+
+namespace Functional.Blazor.Components.Tests
+{
+	public class ResultMatchTests
+	{
+		private const int SUCCESS = 1337;
+		private const string FAILURE = "error";
+
+		[Fact]
+		public void DisplaysSuccessValueWhenResultIsSuccess()
+		{
+			using var context = new TestContext();
+			var cut = context.RenderComponent<ResultMatch<int, string>>(parameters => parameters
+				.Add(c => c.Result, Result.Success<int, string>(SUCCESS))
+				.Add(c => c.Success, i => $"<p>{i}</p>")
+				.Add(c => c.Failure, s => $"<p>{s}</p>"));
+
+			cut.Markup.Should().Contain(SUCCESS.ToString());
+		}
+
+		[Fact]
+		public void DisplaysFailureValueWhenResultIsFaulted()
+		{
+			using var context = new TestContext();
+			var cut = context.RenderComponent<ResultMatch<int, string>>(parameters => parameters
+				.Add(c => c.Result, Result.Failure<int, string>(FAILURE))
+				.Add(c => c.Success, i => $"<p>{i}</p>")
+				.Add(c => c.Failure, s => $"<p>{s}</p>"));
+
+			cut.Markup.Should().Contain(FAILURE);
+		}
+	}
+}
